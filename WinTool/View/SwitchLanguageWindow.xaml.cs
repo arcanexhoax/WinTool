6e0b60bc3b6f33089e51986e539354ca4a4b56e0
hoax <ninjaa176@gmail.com>
2025-05-07 23:26:17 +0300

    show/hide popup animated

--- WinTool/View/SwitchLanguageWindow.xaml.cs
+++ WinTool/View/SwitchLanguageWindow.xaml.cs
@@ -2,28 +2,36 @@
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Interop;
+using System.Windows.Media.Animation;
 using WinTool.Native;
 using WinTool.Utils;
 using WinTool.ViewModel;
+using Timer = System.Timers.Timer;
 
 namespace WinTool.View
 {
     public partial class SwitchLanguageWindow : Window
     {
+        private Timer _hideTimer = new(1500) { AutoReset = false };
+
         public SwitchLanguageWindow(SwitchLanguageViewModel vm)
         {
             InitializeComponent();
             DataContext = vm;
 
+            _hideTimer.Elapsed += (s, e) => Dispatcher.Invoke(HidePopup);
+
             vm.ShowPopup += caretPos =>
             {
-                // https://stackoverflow.com/questions/1918877/how-can-i-get-the-dpi-in-wpf
                 var dpiAtPoint = DpiUtils.GetDpiForNearestMonitor(caretPos.X, caretPos.Y);
                 Left = caretPos.X * DpiUtils.DefaultDpiX / dpiAtPoint;
                 Top = caretPos.Y * DpiUtils.DefaultDpiY / dpiAtPoint;
 
                 ShiftWindowToScreen();
-                Show();
+                ShowPopup();
+
+                _hideTimer.Stop();
+                _hideTimer.Start();
             };
         }
 
@@ -33,6 +41,42 @@ namespace WinTool.View
 
             var hwnd = new WindowInteropHelper(this).Handle;
             NativeMethods.ShowWindowAsPopup(hwnd);
+        }
+
+        private void ShowPopup()
+        {
+            if (Visibility == Visibility.Visible)
+                return;
+
+            Show();
+
+            var growAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 30,
+                Duration = TimeSpan.FromMilliseconds(250),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            BeginAnimation(HeightProperty, growAnimation);
+        }
+
+        private void HidePopup()
+        {
+            if (Visibility == Visibility.Hidden)
+                return;
+
+            var shrinkAnimation = new DoubleAnimation
+            {
+                From = ActualHeight,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(250),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseIn }
+            };
+
+            shrinkAnimation.Completed += (s, e) => Hide();
+
+            BeginAnimation(HeightProperty, shrinkAnimation);
         }
 
         public void ShiftWindowToScreen()
