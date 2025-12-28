5632dd3fe2b1c7637913ba6d96cdf3f5402b336f
hoax <ninjaa176@gmail.com>
2025-12-28 15:55:12 +0100

    add transparent titlebar

--- src/WinTool.WinUI/WinTool.WinUI/Views/MainWindow.xaml.cs
+++ src/WinTool.WinUI/WinTool.WinUI/Views/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Windowing;
 using WinTool.ViewModels;
 
 namespace WinTool.Views;
@@ -7,6 +8,20 @@ public sealed partial class MainWindow : WindowBase
     public MainWindow(MainViewModel vm)
     {
         InitializeComponent();
+
         DataContext = vm;
+
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(titleBar);
+
+        AppWindow.Resize(new Windows.Graphics.SizeInt32(1000, 600));
+        AppWindow.SetIcon("Resources/icon.ico");
+        AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+
+        var presenter = OverlappedPresenter.Create();
+        presenter.PreferredMinimumWidth = 900;
+        presenter.PreferredMinimumHeight = 400;
+
+        AppWindow.SetPresenter(presenter);
     }
 }
