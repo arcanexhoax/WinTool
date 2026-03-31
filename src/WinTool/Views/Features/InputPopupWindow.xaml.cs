2b0a8b2953b0940ed812db576a29643081b9191f
hoax <ninjaa176@gmail.com>
2026-03-31 15:26:19 +0200

    add animated transition between keyboard layouts

--- src/WinTool/Views/Features/InputPopupWindow.xaml.cs
+++ src/WinTool/Views/Features/InputPopupWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using GlobalKeyInterceptor;
 using GlobalKeyInterceptor.Utils;
 using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
 using System.Windows.Media.Animation;
 using WinTool.Native;
 using WinTool.Utils;
@@ -13,12 +15,16 @@ namespace WinTool.Views.Features;
 
 public partial class InputPopupWindow : FluentWindow
 {
-    private const double AnimTimeMs = 200;
-    private const double OffsetY = 20;
+    private const double AppearAnimTimeMs = 200;
+    private const double AppearOffsetY = 20;
+    private const double SelectionAnimTimeMs = 150;
+    public const double SelectionItemWidth = 48;
 
     private readonly IKeyInterceptor _keyInterceptor;
+    private readonly InputPopupViewModel _viewModel;
     private readonly Timer _hideTimer = new(1500) { AutoReset = false };
 
+    private int _lastIndicatorIndex = -1;
     private bool _isHiding;
     private bool _flippedAbove;
     private double _animatedTop;
@@ -30,6 +36,7 @@ public partial class InputPopupWindow : FluentWindow
         InitializeComponent();
         DataContext = vm;
 
+        _viewModel = vm;
         _keyInterceptor = keyInterceptor;
         _keyInterceptor.ShortcutPressed += OnShortcutPressed;
         _hideTimer.Elapsed += (s, e) => Dispatcher.Invoke(HidePopup);
@@ -40,11 +47,15 @@ public partial class InputPopupWindow : FluentWindow
             {
                 _hideTimer.Stop();
 
+                UpdateSelectionIndicatorPosition();
+
                 var dpiAtPoint = DpiUtils.GetDpiForNearestMonitor(caretPos.X, caretPos.Y);
                 var x = caretPos.X * DpiUtils.DefaultDpiX / dpiAtPoint;
                 var y = caretPos.Y * DpiUtils.DefaultDpiY / dpiAtPoint;
 
                 ShowPopup(x, y);
+
+                UpdateSelectionIndicatorPosition();
 
                 _hideTimer.Start();
             });
@@ -77,11 +88,11 @@ public partial class InputPopupWindow : FluentWindow
         if (Visibility == Visibility.Visible && _currentHideAnimGuid == Guid.Empty && (x, y) == _lastPosition)
             return;
 
-        var (finalTop, flipped) = AdjustToScreen(x, y, Height);
+        var (finalTop, flipped) = AdjustWindowToScreen(x, y, Height);
         _flippedAbove = flipped;
         _animatedTop = finalTop;
 
-        var animFrom = flipped ? finalTop + OffsetY : finalTop - OffsetY;
+        var animFrom = flipped ? finalTop + AppearOffsetY : finalTop - AppearOffsetY;
         Top = animFrom;
 
         _lastPosition = (x, y);
@@ -95,7 +106,7 @@ public partial class InputPopupWindow : FluentWindow
         {
             From = animFrom,
             To = finalTop,
-            Duration = TimeSpan.FromMilliseconds(AnimTimeMs),
+            Duration = TimeSpan.FromMilliseconds(AppearAnimTimeMs),
             EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
         };
 
@@ -110,13 +121,13 @@ public partial class InputPopupWindow : FluentWindow
         _isHiding = true;
 
         var animGuid = _currentHideAnimGuid = Guid.NewGuid();
-        var hideTo = _flippedAbove ? _animatedTop + OffsetY : _animatedTop - OffsetY;
+        var hideTo = _flippedAbove ? _animatedTop + AppearOffsetY : _animatedTop - AppearOffsetY;
 
         var slide = new DoubleAnimation
         {
             From = _animatedTop,
             To = hideTo,
-            Duration = TimeSpan.FromMilliseconds(AnimTimeMs),
+            Duration = TimeSpan.FromMilliseconds(AppearAnimTimeMs),
             EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseIn }
         };
 
@@ -135,7 +146,80 @@ public partial class InputPopupWindow : FluentWindow
         BeginAnimation(TopProperty, slide);
     }
 
-    private (double FinalTop, bool FlippedAbove) AdjustToScreen(double caretX, double caretY, double height)
+    private void UpdateSelectionIndicatorPosition()
+    {
+        if (_viewModel.AllLanguages is null or [])
+            return;
+
+        int selectedIndex = -1;
+
+        foreach (var (i, lang) in _viewModel.AllLanguages.Index())
+        {
+            if (lang.IsSelected)
+            {
+                selectedIndex = i;
+                break;
+            }
+        }
+
+        if (selectedIndex < 0)
+            return;
+
+        if (_lastIndicatorIndex >= _viewModel.AllLanguages.Count)
+            _lastIndicatorIndex = -1;
+
+        var (targetX, itemWidth) = GetSelectionItemMetrics(selectedIndex);
+
+        if (itemWidth > 0)
+            SelectionOverlay.Width = itemWidth;
+
+        if (_lastIndicatorIndex < 0 || Visibility != Visibility.Visible || _isHiding)
+        {
+            SelectionTranslate.BeginAnimation(TranslateTransform.XProperty, null);
+            AccentScale.BeginAnimation(ScaleTransform.ScaleXProperty, null);
+
+            SelectionTranslate.X = targetX;
+            AccentScale.ScaleX = 1.0;
+
+            _lastIndicatorIndex = selectedIndex;
+            return;
+        }
+
+        if (selectedIndex == _lastIndicatorIndex)
+            return;
+
+        _lastIndicatorIndex = selectedIndex;
+        AnimateSelectionIndicator(targetX);
+    }
+
+    private (double X, double Width) GetSelectionItemMetrics(int index)
+    {
+        if (LanguageItemsControl.ItemContainerGenerator.ContainerFromIndex(index) is FrameworkElement container)
+        {
+            var pos = container.TransformToAncestor(LanguageItemsControl).Transform(new Point(0, 0));
+            return (pos.X, container.ActualWidth);
+        }
+
+        return (index * SelectionItemWidth, SelectionItemWidth);
+    }
+
+    private void AnimateSelectionIndicator(double targetX)
+    {
+        var duration = TimeSpan.FromMilliseconds(SelectionAnimTimeMs);
+
+        var slideAnim = new DoubleAnimationUsingKeyFrames { Duration = duration };
+        slideAnim.KeyFrames.Add(new SplineDoubleKeyFrame(targetX, KeyTime.FromPercent(1.0), new KeySpline(0.8, 0, 0.2, 1)));
+
+        var squeezeAnim = new DoubleAnimationUsingKeyFrames { Duration = duration };
+        squeezeAnim.KeyFrames.Add(new SplineDoubleKeyFrame(0.6, KeyTime.FromPercent(0.167), new KeySpline(0, 0, 0, 1)));
+        squeezeAnim.KeyFrames.Add(new LinearDoubleKeyFrame(0.6, KeyTime.FromPercent(0.667)));
+        squeezeAnim.KeyFrames.Add(new SplineDoubleKeyFrame(1.0, KeyTime.FromPercent(1.0), new KeySpline(0, 0, 0, 1)));
+
+        SelectionTranslate.BeginAnimation(TranslateTransform.XProperty, slideAnim);
+        AccentScale.BeginAnimation(ScaleTransform.ScaleXProperty, squeezeAnim);
+    }
+
+    private (double FinalTop, bool FlippedAbove) AdjustWindowToScreen(double caretX, double caretY, double height)
     {
         var width = ActualWidth;
 
